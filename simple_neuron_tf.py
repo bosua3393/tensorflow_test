0042be6777510edbd95be3fb0161f0d7c7c77acc
bosua3393@gmail.com <bosua3393@gmail.com>
2019-03-13 01:32:58 -0400

    modified

--- simple_neuron_tf.py
+++ simple_neuron_tf.py
@@ -16,33 +16,37 @@ train_data = np.array([[1, 0, 1, 1, 0, 0],
 
 train_output = np.array([[1], [1], [1], [1], [0], [1], [0], [0], [1], [1], [0], [0]])
 
-test_data = np.array([[1, 0, 1, 0, 1, 1],
-                      [1, 0, 0, 1, 0, 0],
-                      [0, 1, 0, 1, 1, 0],
-                      [1, 0, 1, 0, 1, 0],
-                      [0, 1, 0, 1, 1, 1]])
+test_data = np.array([[0, 0, 1, 0, 1, 1]])
 
 test_output = np.array([[1], [1], [0], [1], [0]])
 
 b = tf.Variable(tf.zeros(1))
 w = tf.Variable(tf.random_uniform((6, 1), -1, 1))
 
-x = tf.placeholder(tf.float32, (12, 6))
+x = tf.placeholder(tf.float32, (1, 6))
 
 y = tf.nn.sigmoid(tf.matmul(x, w) + b)
 
-label = tf.placeholder(tf.float32, [12, 1])
+label = tf.placeholder(tf.float32, [1, 1])
 
 loss = (y - label) * (y - label)
 
-sess = tf.Session()
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 sess.run(tf.global_variables_initializer())
 
-train_step = tf.train.GradientDescentOptimizer(.01).minimize(loss)
+train_step = tf.train.GradientDescentOptimizer(20).minimize(loss)
 
-for i in range(10000):
-    sess.run(train_step, feed_dict={x: train_data, label: train_output})
+for i in range(1000):
+    batch_x = np.array_split(train_data, len(train_data))
+    batch_label = np.array_split(train_output, len(train_output))
+    loss_value = 0.0
+    for batch in range(len(batch_x)):
+        sess.run(train_step, feed_dict={x: batch_x[batch], label: batch_label[batch]})
+        if i % 100 == 0:
+            loss_value += sess.run(loss, feed_dict={x: batch_x[batch], label: batch_label[batch]})
+    if i % 100 == 0:
+        print(loss_value/len(batch_x))
 
-prediction = sess.run(y, {x: train_data})
+prediction = sess.run(y, {x: test_data})
 
-print(np.round(prediction))
+print(prediction)
