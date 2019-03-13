a01d787393331de8c2bacf3133d7fb45d8e1b659
yui-sub <bosua3393@gmail.com>
2019-03-13 15:42:41 -0400

    added tensorboard

--- simple_neuron_tf.py
+++ simple_neuron_tf.py
@@ -24,19 +24,19 @@ test_data = np.array([[1, 0, 1, 0, 1, 1],
 
 test_output = np.array([[1], [1], [0], [1], [0]])
 
-b = tf.Variable(tf.zeros(1))
-w = tf.Variable(tf.random_uniform((6, 1), -1, 1))
+x = tf.placeholder(tf.float32, (1, 6), name="x")
 
-x = tf.placeholder(tf.float32, (1, 6))
+with tf.name_scope(name="layer1"):
+    b = tf.Variable(tf.zeros(1), name="bias")
+    w = tf.Variable(tf.random_uniform((6, 1), -1, 1), name="weights")
+    y = tf.nn.sigmoid(tf.matmul(x, w) + b, name="y")
 
-y = tf.nn.sigmoid(tf.matmul(x, w) + b)
-
-label = tf.placeholder(tf.float32, [1, 1])
-
-loss = (y - label) * (y - label)
+label = tf.placeholder(tf.float32, [1, 1], name="label")
+loss = tf.square(y - label, name="loss")
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
+writer = tf.summary.FileWriter('./graphs', graph=tf.get_default_graph())
 
 train_step = tf.train.GradientDescentOptimizer(25).minimize(loss)
 
@@ -44,18 +44,17 @@ for i in range(1000):
     batch_x = np.array_split(train_data, len(train_data))
     batch_label = np.array_split(train_output, len(train_output))
     loss_value = 0.0
+
     for batch in range(len(batch_x)):
         sess.run(train_step, feed_dict={x: batch_x[batch], label: batch_label[batch]})
         if i % 100 == 0:
             loss_value += sess.run(loss, feed_dict={x: batch_x[batch], label: batch_label[batch]})
+
     if i % 100 == 0:
         print(loss_value / len(batch_x))
+
 
 print('testing')
 test = np.array_split(test_data, len(test_data))
 for i in range(len(test_data)):
     print(np.round(sess.run(y, {x: test[i]}), 3))
-print('weights')
-print(sess.run(w))
-print('bias')
-print(sess.run(b))
